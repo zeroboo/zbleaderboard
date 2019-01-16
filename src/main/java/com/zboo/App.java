b93b1a4687bbddb213211e31d01c83971d4049e8
Pham Tan Hung <tanhung.pham@nhnent.com>
2019-01-16 17:25:03 +0700

    init code for leaderboard service

--- src/main/java/com/zboo/App.java
+++ src/main/java/com/zboo/App.java
@@ -1,5 +1,10 @@
 package com.zboo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.zboo.leaderboard.LeaderboardService;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +13,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        final Logger logger = LoggerFactory.getLogger(App.class);
+
+        logger.info( "Starting!" );
+        final LeaderboardService service = new LeaderboardService();
+        Runtime.getRuntime().addShutdownHook(new Thread()
+        {
+            public void run()
+            {
+                logger.info("Application Terminating ...");
+                service.stop();
+                logger.info("Application Terminated ...");
+            }
+        });
     }
 }
---
+++ src/main/java/com/zboo/leaderboard/LeaderboardService.java
@@ -0,0 +1,71 @@
+package com.zboo.leaderboard;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import javax.net.ssl.SSLException;
+import java.security.cert.CertificateException;
+
+public class LeaderboardService {
+
+    public LeaderboardService() {
+
+    }
+
+    boolean SSL = System.getProperty("ssl") != null;
+    int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8443" : "8080"));
+    EventLoopGroup bossGroup = null;
+    EventLoopGroup workerGroup = null;
+
+    public void start() throws CertificateException, SSLException, InterruptedException {
+        // Configure SSL.
+        final SslContext sslCtx;
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
+        } else {
+            sslCtx = null;
+        }
+
+        // Configure the server.
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new LeaderboardServiceInitializer(sslCtx));
+
+            Channel ch = b.bind(PORT).sync().channel();
+
+            System.err.println("Open your web browser and navigate to " +
+                    (SSL ? "https" : "http") + "://127.0.0.1:" + PORT + '/');
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public void stop() {
+        if (bossGroup != null) {
+            bossGroup.shutdownGracefully();
+        }
+
+        if (workerGroup != null) {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
